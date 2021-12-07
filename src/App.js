226cbd7e3c1051e5b224840d8fe884ff03587b8f
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-07 14:51:10 -0800

    Project setup, added routes

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import './App.css'
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipe/:id">
+            <Recipe />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
