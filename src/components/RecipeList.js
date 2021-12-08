120d0ee5aa1130684cba22e0518f8f66a928d778
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-08 13:52:02 -0800

    Added alternative background

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,6 +2,11 @@ import './RecipeList.css'
 import { Link } from 'react-router-dom'
 
 export default function RecipeList({ recipes }) {
+
+  if (recipes.length === 0) {
+    return <div style={{textAlign: "center"}}>No recipes found...</div>
+  }
+
   return (
     <div className="recipe-list">
       {recipes.map(recipe => (
