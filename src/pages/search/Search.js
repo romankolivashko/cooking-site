f3061ff1e0598852ef7446fb5fe68b9de2f9d9ad
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-08 11:34:54 -0800

    Added search output result

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,9 +1,22 @@
+import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
 import './Search.css'
 
 export default function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q')
+
+  const url =  'http://localhost:3002/recipes?q=' + query
+  const { error, isPending, data } = useFetch(url)
+
   return (
     <div>
-      Search
+      <h2 className="page-title">Recipe including "{query}"</h2>
+      {error && <p className="error"></p>}
+      {isPending && <p className="loading">Loading ...</p>}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
